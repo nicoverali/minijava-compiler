b07843c5e7f65cfee4e61fd19c16da7a3c3efbd0
Nicolás A. Vera <nicolasalevera98@gmail.com>
2020-11-23 01:59:19 -0300

    Add methods to Iterables and add HashMultimap class

--- src/main/java/util/Iterables.java
+++ src/main/java/util/Iterables.java
@@ -1,21 +1,55 @@
 package util;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Iterables {
 
-    public static <T> T getLast(Iterable<T> iterable){
-        T last = null;
-        for (T element : iterable){
-            last = element;
-        }
-        return last;
+    /**
+     * Returns the first element in {@code iterable}.
+     *
+     * @return the first element of {@code iterable} or the default value
+     * @throws NoSuchElementException if the iterable is empty
+     */
+    public static <T> T getFirst(Iterable<T> iterable) throws NoSuchElementException{
+        return iterable.iterator().next();
     }
 
-    public static <T> T getLast(Iterable<T> iterable, T defaultValue){
-        T last = defaultValue;
-        for (T element : iterable){
-            last = element;
+    /**
+     * Returns the first element in {@code iterable} or {@code defaultValue} if the iterable is empty.
+     *
+     * @param defaultValue the default value to return if the iterable is empty
+     * @return the first element of {@code iterable} or the default value
+     */
+    public static <T> T getFirst(Iterable<T> iterable, T defaultValue){
+        Iterator<T> iterator = iterable.iterator();
+        return iterator.hasNext() ? iterator.next() : defaultValue;
+    }
+
+    /**
+     * Returns the last element of {@code iterable}.
+     *
+     * @return the last element of {@code iterable}
+     * @throws NoSuchElementException if the iterable is empty
+     */
+    public static <T> T getLast(Iterable<T> iterable) throws NoSuchElementException {
+        Iterator<T> iterator = iterable.iterator();
+        while (true) {
+            T current = iterator.next();
+            if (!iterator.hasNext()) {
+                return current;
+            }
         }
-        return last;
+    }
+
+    /**
+     * Returns the last element of {@code iterable} or {@code defaultValue} if the iterable is empty.
+     *
+     * @param defaultValue the value to return if {@code iterable} is empty
+     * @return the last element of {@code iterable} or the default value
+     */
+    public static <T> T getLast(Iterable<T> iterable, T defaultValue){
+        return iterable.iterator().hasNext() ? getLast(iterable) : defaultValue;
     }
 
 
