8319102fed337edbb052552d070e3578156ae157
Nicolás A. Vera <nicolasalevera98@gmail.com>
2021-10-10 17:18:19 -0300

    Fix LexicalErrorPrinter

--- src/main/java/error/LexicalErrorPrinter.java
+++ src/main/java/error/LexicalErrorPrinter.java
@@ -1,55 +1,21 @@
 package error;
 
 import lexical.LexicalException;
-import util.Characters;
 
 import static java.lang.System.out;
 
-public class LexicalErrorPrinter {
-
-    private static final String DETAIL_PREFIX = "Detalle: ";
-    private static final String INITIAL_PADDING = padLeft("", DETAIL_PREFIX.length());
+public class LexicalErrorPrinter extends AbstractErrorPrinter {
 
     public void printError(LexicalException exception){
         printDescription(exception);
-        printDetail(exception);
-        printErrorCode(exception);
+        printDetail(exception.getLine(), exception.getColumnNumber());
+        printErrorCode(exception.getLexeme(), exception.getLineNumber());
     }
 
     private void printDescription(LexicalException exception){
         int line = exception.getLineNumber()+1;
-        int column = exception.getColumnNumber()+1;
         String errorMsg = exception.getMessage();
-        out.println("Error Léxico en linea "+line+", columna "+ column +": "+errorMsg);
-    }
-
-    private void printDetail(LexicalException exception){
-        String line = Characters.removeNewLine(exception.getLine());
-        String indicator = createIndicator(line, exception.getColumnNumber());
-
-        out.println(DETAIL_PREFIX+line);
-        out.println(indicator);
-    }
-
-    private String createIndicator(String line, int indicatorColumn){
-        StringBuilder indicator = new StringBuilder(INITIAL_PADDING);
-        for (int i = 0; i < indicatorColumn; i++) {
-            if (line.charAt(i) == '\t')
-                indicator.append('\t');
-            else
-                indicator.append(' ');
-        }
-        return indicator.append('^').toString();
-    }
-
-    private void printErrorCode(LexicalException exception){
-        int line = exception.getLineNumber()+1;
-        String lexeme = exception.getLexeme();
-        out.println("[Error:"+lexeme+"|"+line+"]");
-    }
-
-    private static String padLeft(String s, int n) {
-        return String.format("%" + n + "s", s);
+        out.println("Error Léxico en linea "+line+": "+errorMsg);
     }
 
 
