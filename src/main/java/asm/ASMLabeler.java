6d01c573db6608ad244d5b45f27efd560093087f
Nicolas A. Vera <nicolasalevera98@gmail.com>
2021-12-01 16:17:43 -0300

    Improve ASMLabeler

--- src/main/java/asm/ASMLabeler.java
+++ src/main/java/asm/ASMLabeler.java
@@ -1,30 +1,28 @@
 package asm;
 
-import semantic.symbol.AttributeSymbol;
-import semantic.symbol.ClassSymbol;
-import semantic.symbol.ConstructorSymbol;
-import semantic.symbol.MethodSymbol;
+import semantic.symbol.*;
 import semantic.symbol.attribute.type.Type;
 
 import java.util.stream.Collectors;
 
 public class ASMLabeler {
 
+    private static int nextLabel = 0;
+
+    public static String label(String prefix) {
+        return String.format("l_%s_%s", prefix, nextLabel++);
+    }
+
     public static String label(AttributeSymbol attr) {
         String container = attr.getContainer() != null ? attr.getContainer().getName() : "NONE";
         return String.format("%s@%s", attr.getName(), container);
     }
 
-    public static String label(MethodSymbol method) {
-        String params = method.getParametersTypes().stream().map(Type::toString).collect(Collectors.joining("$"));
-        String container = method.getContainer() != null ? method.getContainer().getName() : "NONE";
-        return String.format("%s$%s@%s", method.getName(), params, container);
-    }
+    public static String label(CallableSymbol callable) {
+        String params = callable.getParametersTypes().stream().map(Type::toString).collect(Collectors.joining("$"));
+        String clazz = callable.getContainer() != null ? callable.getContainer().getName() : "NONE";
 
-    public static String label(ConstructorSymbol constructor) {
-        String params = constructor.getParametersTypes().stream().map(Type::toString).collect(Collectors.joining("$"));
-        String clazz = constructor.getContainer() != null ? constructor.getContainer().getName() : "NONE";
-        return String.format("constructor$%s@%s", params, clazz);
+        return String.format("%s$%s@%s", callable.getName(), params, clazz);
     }
 
     public static String labelVT(ClassSymbol classSymbol) {
